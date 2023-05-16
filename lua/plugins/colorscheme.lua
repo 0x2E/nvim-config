3847b8e3a4106161617fee9c6e815d5a3c1aa681
rook1e <rook1e404@outlook.com>
2023-05-16 12:51:58 +0800

    commonly used LSP and colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    -- lazy = true,
+    name = "tokyonight",
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    -- lazy = true,
+    name = "kanagawa",
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    -- lazy = true,
+    name = "github",
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      single_file_support = true,
+    },
+  },
+}
