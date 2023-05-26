6abd4258cc7a0fb430a2c500c080c31f93b004b2
rook1e <rook1e404@outlook.com>
2023-05-27 00:24:00 +0800

    refactor `0` keymap for better jump

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set({ "i" }, "jj", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+vim.keymap.set("i", "jj", "<esc>", { desc = "Exit insert mode" })
 
 vim.keymap.set("n", "<leader>xf", vim.diagnostic.open_float, { desc = "Pop-up error messages" })
+
+-- Jump to the first char or the begginning of the line
+local function jump_line_0()
+  local head = (vim.api.nvim_get_current_line():find("[^%s]") or 1) - 1
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  cursor[2] = cursor[2] == head and 0 or head
+  vim.api.nvim_win_set_cursor(0, cursor)
+end
+-- vim.keymap.set({'i', 'n'}, '<Home>', home)
+vim.keymap.set("n", "0", jump_line_0, { desc = "Jump to the first char or the begginning of the line" })
